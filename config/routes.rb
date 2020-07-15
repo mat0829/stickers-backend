3506849f82d74a23191dc2c93e046ccdb9a6d5f7
mat_anderson <matandersonlv@hotmail.com>
2020-07-15 16:25:57 -0700

    configured routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+      resources :task_images
+      resources :stickers
+      resources :prizes
+      resources :prize_images
+      resources :users, only: [:create, :update, :destroy]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 end
